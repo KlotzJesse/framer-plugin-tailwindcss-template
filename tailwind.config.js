a38e58408406cd567da94d7b8bf27aa7b2adc36b
KlotzJesse <jesseklotz2306@gmail.com>
2024-11-22 18:59:25 +0100

    bug: remove wrong added text 'bun'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", bun"./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: ["selector", '[data-framer-theme="dark"]'],
   theme: {
     extend: {},
